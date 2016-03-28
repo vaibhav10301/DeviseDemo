f034de9381f388d361adc9e9e18fd56498e3edbf
vaibhav10301 <agrawal10301@gmail.com>
2016-03-29 00:12:46 +0530

    Authorization with cancan applied in view very basic

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'homes#index'
   resources :homes
   devise_for :users , :controllers => { registrations: 'registrations' }
-    get 'homes/destroy/:id', to:'homes#destroy', as:'homes_destroy'
+  get 'homes/destroy/:id', to:'homes#destroy', as:'homes_destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160319202032) do
+ActiveRecord::Schema.define(version: 20160328182457) do
 
   create_table "homes", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160319202032) do
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
     t.string   "name",                   limit: 255
+    t.boolean  "Role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
